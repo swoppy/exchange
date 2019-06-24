9852d97b8d5d6b8cff717954a23977f0e3fc0886
swoppy <x>
2019-06-24 20:52:18 +0800

    order book components

--- src/app/components/Body.js
+++ src/app/components/Body.js
@@ -21,13 +21,235 @@ export class Body extends React.Component {
                                 header
                             </div>
                             <div className={css(styles.sellOrderBook)}>
-                                sell orders
+                                <div className={css(styles.sellOrderHeader)}>
+                                    <div className={css(styles.priceHeader)}>
+                                        Price
+                                    </div>
+                                    <div className={css(styles.amountHeader)}>
+                                        Amount
+                                    </div>
+                                    <div className={css(styles.totalHeader)}>
+                                        Total
+                                    </div>
+                                </div>
+                                <div className={css(styles.mainSellOrderCon)}>
+                                    <div className={css(styles.sellOrderRowCon)}>
+                                        <div className={css(styles.sellPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.sellOrderRowCon)}>
+                                        <div className={css(styles.sellPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.sellOrderRowCon)}>
+                                        <div className={css(styles.sellPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.sellOrderRowCon)}>
+                                        <div className={css(styles.sellPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.sellOrderRowCon)}>
+                                        <div className={css(styles.sellPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.sellOrderRowCon)}>
+                                        <div className={css(styles.sellPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.sellOrderRowCon)}>
+                                        <div className={css(styles.sellPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.sellOrderRowCon)}>
+                                        <div className={css(styles.sellPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.sellOrderRowCon)}>
+                                        <div className={css(styles.sellPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                             <div className={css(styles.currentPrice)}>
-                                current price
+                                10000.93225
                             </div>
                             <div className={css(styles.buyOrderBook)}>
-                                buy order book
+                            <div className={css(styles.mainBuyOrderCon)}>
+                                    <div className={css(styles.buyOrderRowCon)}>
+                                        <div className={css(styles.buyPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.buyOrderRowCon)}>
+                                        <div className={css(styles.buyPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.buyOrderRowCon)}>
+                                        <div className={css(styles.buyPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.buyOrderRowCon)}>
+                                        <div className={css(styles.buyPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.buyOrderRowCon)}>
+                                        <div className={css(styles.buyPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.buyOrderRowCon)}>
+                                        <div className={css(styles.buyPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.buyOrderRowCon)}>
+                                        <div className={css(styles.buyPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.buyOrderRowCon)}>
+                                        <div className={css(styles.buyPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.buyOrderRowCon)}>
+                                        <div className={css(styles.buyPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                    <div className={css(styles.buyOrderRowCon)}>
+                                        <div className={css(styles.buyPrice)}>
+                                            10000.93
+                                        </div>
+                                        <div className={css(styles.amount)}>
+                                            90.909467
+                                        </div>
+                                        <div className={css(styles.total)}>
+                                            13.0004456
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div className={css(styles.orderBookHistory)}>
@@ -47,13 +269,14 @@ const styles = StyleSheet.create({
     body: {
         color: 'white',
         display: 'flex',
-        flexDirection: 'row'
+        flexDirection: 'row',
+        position: 'relative',
+        fontSize: '1vw'
     },
     chartAndHistoryCon: {
         display: 'flex',
         flexDirection: 'column',
         border: '1px solid rgb(0, 0, 0)',
-        position: 'relative',
         width: '60vw',
         backgroundColor: 'rgb(38, 45, 51)'
     },
@@ -71,7 +294,7 @@ const styles = StyleSheet.create({
     orderBookCon: {
         display: 'flex',
         flexDirection: 'column',
-        border: '1px solid rgb(0, 0, 0)',
+        //border: '1px solid rgb(0, 0, 0)',
         position: 'relative',
         width: '40vw',
         backgroundColor: 'rgb(38, 45, 51)',
@@ -80,24 +303,72 @@ const styles = StyleSheet.create({
     orderBookWrapper: {
         display: 'flex',
         flexDirection: 'row',
-        border: '1px solid rgb(0, 0, 0)',
         height: '60vh',
     },
     orderBook: {
         display: 'flex',
         flexDirection: 'column',
         border: '1px solid rgb(0, 0, 0)',
-        width: '70%',
+        width: '35vw',
         position: 'relative'
     },
     orderBookHeader: {
         width: '100%',
-        border: '1px solid rgb(0, 0, 0)',
+        //border: '1px solid rgb(0, 0, 0)',
     },
     sellOrderBook: {
+        display: 'flex',
+        flexDirection: 'column',
         width: '100%',
         height: '27vh',
-        border: '1px solid rgb(0, 0, 0)'
+    },
+    sellOrderHeader: {
+        display: 'flex',
+        flexDirection: 'row',
+        textAlign: 'center'
+    },
+    priceHeader: {
+        flexGrow: '1'
+    },
+    amountHeader: {
+        flexGrow: '1',
+    },
+    totalHeader: {
+        flexGrow: '1',
+    },
+    mainSellOrderCon: {
+        
+    },
+    sellOrderRowCon: {
+        display: 'flex',
+        flexDirection: 'row',
+        textAlign: 'right',
+        color: ''
+    },
+    buyOrderRowCon: {
+        display: 'flex',
+        flexDirection: 'row',
+        textAlign: 'right'
+    },
+    sellPrice: {
+        flexGrow: '1',
+        textAlign: 'left', 
+        marginLeft: '6px',
+        color: 'rgb(255, 0, 122)'
+    },
+    buyPrice: {
+        flexGrow: '1',
+        textAlign: 'left', 
+        marginLeft: '6px',
+        color: 'rgb(112, 168, 0)'
+    },
+    amount: {
+        flexGrow: '1',
+        color: 'rgb(212, 212, 212)'
+    },
+    total: {
+        flexGrow: '1',
+        color: 'rgb(212, 212, 212)'
     },
     currentPrice: {
         display: 'flex',

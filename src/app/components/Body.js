75525372311619339a9ab692648fd56102b4dfa9
swoppy <x>
2019-07-01 18:45:22 +0800

    body stuff, add icons

--- src/app/components/Body.js
+++ src/app/components/Body.js
@@ -19,12 +19,27 @@ export class Body extends React.Component {
                         <div className={css(styles.orderBook)}>
                             <div className={css(styles.orderBookHeader)}>
                                 <ul className={css(styles.ul)}>
-                                    <li>B&S</li>
-                                    <li>B</li>
-                                    <li>S</li>
+                                    <li>
+                                        <input type='image'
+                                                src={require('../assets/img/b&s.svg')}
+                                                className={css(styles.buySellLogo)}
+                                        />
+                                    </li>
+                                    <li>
+                                        <input type='image'
+                                                src={require('../assets/img/buy.svg')}
+                                                className={css(styles.buySellLogo)}
+                                        />
+                                    </li>
+                                    <li>
+                                    <input type='image'
+                                                src={require('../assets/img/sell.svg')}
+                                                className={css(styles.buySellLogo)}
+                                        />
+                                    </li>
                                 </ul>
                                 <div className={css(styles.decimalsCon)}>
-                                    <label>Group</label>
+                                    <label>groups</label>
                                     <select className={css(styles.select)}>
                                         <option>2 decimals</option>
                                         <option>4 decimals</option>
@@ -332,14 +347,35 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row',
         flexWrap: 'nowrap',
+        whiteSpace: 'nowrap',
         justifyContent: 'space-between',
         flex: '0 0 auto',
         padding: '4px'
     },
         ul: {
             listStyleType: 'none',
-            display: 'flex'
+            display: 'flex',
+            margin: '0',
+            padding: '0'
         },
+            buySellLogo: {
+                width: '30px',
+                height: '22px',
+                //backgroundColor: 'rgb(47, 56, 63)',
+                //color: 'rgb(245, 188, 0)',
+                cursor: 'pointer',
+                lineHeight: '100%',
+                fontSize: '12px',
+                borderWidth: '1px',
+                borderStyle: 'solid',
+                borderImage: 'initial',
+                outline: '0px',
+                margin: '0px !important',
+                padding: '0.1em !important',
+                ':active': {
+                    borderColor: 'rgb(240, 185, 11)'
+                }
+            },
         select: {
             backgroundColor: 'rgb(47, 56, 63)',
             cursor: 'pointer',
@@ -351,7 +387,8 @@ const styles = StyleSheet.create({
             borderImage: 'initial',
             margin: '0px 2px',
             outline: '0px',
-            padding: '2px 3px !important'
+            padding: '2px 3px !important',
+            color: '#999999'
         },
     sellOrderBook: {
         display: 'flex',

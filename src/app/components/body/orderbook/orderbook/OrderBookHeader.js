0e24231f0a9aab47cedb4bd0ca4432a29132abff
swoppy <json.barba@gmail.com>
2019-07-13 16:48:49 +0800

    revert back to class component

--- src/app/components/body/orderbook/orderbook/OrderBookHeader.js
+++ src/app/components/body/orderbook/orderbook/OrderBookHeader.js
@@ -1,59 +1,45 @@
 import React, { useState } from "react"
 import { StyleSheet, css } from 'aphrodite/no-important'
 
-export function OrderBookHeader () {
-    const [logo, setBuyAndSellLogo] = useState([
-        {
-            source: require('../../../../assets/img/b&s.svg'),
-            isToggled: false
-        },
-        {
-            source: require('../../../../assets/img/buy.svg'),
-            isToggled: false
-        },
-        {
-            source: require('../../../../assets/img/sell.svg'),
-            isToggled: false
-        }
+export class OrderBookHeader extends React.Component {
+    render(){
+        return (
+            <div className={css(styles.orderBookHeader)}>
+                <ul className={css(styles.ul)}>
 
-    ])
-    //console.log();
-    return (
-        <div className={css(styles.orderBookHeader)}>
-            <ul className={css(styles.ul)}>
-
-                <li className={css(styles.li)}>
-                    <input  type='image'
-                            src={logo[0].source}
+                    <li className={css(styles.li)}>
+                        <input  type='image'
+                                src={require('../../../../assets/img/b&s.svg')}
+                                className={css(styles.buySellLogo)}
+                                //onClick={this.buyAndSell}
+                        />
+                    </li>
+                    <li className={css(styles.li)}>
+                        <input id='buy'
+                            type='image'
+                            src={require('../../../../assets/img/sell.svg')}
                             className={css(styles.buySellLogo)}
-                            //onClick={this.buyAndSell}
-                    />
-                </li>
-                <li className={css(styles.li)}>
-                    <input id='buy'
-                        type='image'
-                        src={logo[1].source}
-                        className={css(styles.buySellLogo)}
-                    />
-                </li>
-                <li className={css(styles.li)}>
-                <input type='image'
-                        src={logo[2].source}
-                        className={css(styles.buySellLogo)}
-                    />
-                </li>
-            </ul>
-            <div className={css(styles.decimalsCon)}>
-                <label>groups</label>
-                <select className={css(styles.select)}>
-                    <option>2 decimals</option>
-                    <option>4 decimals</option>
-                    <option>5 decimals</option>
-                    <option>7 decimals</option>
-                </select>
+                        />
+                    </li>
+                    <li className={css(styles.li)}>
+                    <input type='image'
+                            src={require('../../../../assets/img/buy.svg')}
+                            className={css(styles.buySellLogo)}
+                        />
+                    </li>
+                </ul>
+                <div className={css(styles.decimalsCon)}>
+                    <label>groups</label>
+                    <select className={css(styles.select)}>
+                        <option>2 decimals</option>
+                        <option>4 decimals</option>
+                        <option>5 decimals</option>
+                        <option>7 decimals</option>
+                    </select>
+                </div>
             </div>
-        </div>
         )
+    }
 }
 const styles = StyleSheet.create({
     orderBookHeader: {

aae44d5666a54a667b18a28b25c0e68192de1d82
swoppy <json.barba@gmail.com>
2019-07-18 16:21:27 +0800

    inital context at buyAndSell component

--- src/app/components/body/orderbook/buyandsell/BuyAndSell.js
+++ src/app/components/body/orderbook/buyandsell/BuyAndSell.js
@@ -10,6 +10,22 @@ export class BuyAndSell extends React.Component {
             <div className={css(styles.buyAndSellCon)}>
                 <Header/>
                 <SubHeader />
+                <div className={css(styles.context)}>
+                    <div className={css(styles.buy)}>
+                        <form>
+                            <div className={css(styles.head)}>
+                                Buy BTC
+                            </div>
+                        </form>
+                    </div>
+                    <div className={css(styles.sell)}>
+                        <form>
+                            <div className={css(styles.head)}>
+                                Sell BTC
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         )
     }
@@ -21,5 +37,27 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         border: '1px solid rgb(0, 0, 0)',
         height: '36vh'
-    }
+    },
+    context: {
+        display: 'flex',
+        flexDirection: 'row',
+        height: '100%',
+        color: 'rgb(212, 212, 212)',
+    },
+        buy: {
+            display: 'flex',
+            flexDirection: 'column',
+            padding: '12px 22px',
+            flex: '1 1 0',
+            borderRight: '1px solid rgb(64, 75, 85)',
+        },
+            head: {
+                //border: '1px solid white'
+            },
+        sell: {
+            display: 'flex',
+            flex: '1 1 0',
+            flexDirection: 'column',
+            padding: '12px 22px',
+        }
 });
